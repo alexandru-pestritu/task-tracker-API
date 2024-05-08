edb5c9448dba22f89de2e120dfe087621d26d10b
alexandru-pestritu <147277107+alexandru-pestritu@users.noreply.github.com>
2024-05-08 11:22:01 +0300

    Added ICollectionService and ITaskCollectionService interfaces

---
+++ Services/ICollectionService.cs
@@ -0,0 +1,12 @@
+﻿namespace TasksAPI.Services
+{
+    public interface ICollectionService<T>
+    {
+        List<T> GetAll();
+        T Get(Guid id);
+        bool Create(T model);
+        bool Update(Guid id, T model);
+        bool Delete(Guid id);
+
+    }
+}
---
+++ Services/ITaskCollectionService.cs
@@ -0,0 +1,9 @@
+﻿using TasksAPI.Models;
+
+namespace TasksAPI.Services
+{
+    public interface ITaskCollectionService : ICollectionService<TaskModel>
+    {
+        List<TaskModel> GetTasksByStatus(string status);
+    }
+}
